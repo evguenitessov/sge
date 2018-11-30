504b1fbc4d4eb041ab040108a5d39e09a9a20de5
evguenitessov <tessov.evgueni@hotmail.com>
2018-11-30 20:46:52 -0300

    7

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -4,12 +4,10 @@ import spark.Spark;
 import spark.debug.DebugScreen;
 
 public class Server {
-	public static void main(String[] args) {
-		Spark.port(9000);
-		DebugScreen.enableDebugScreen();
+	public static void main(String[] args) {		
 		Router.configure();
 		
-		Spark.init();
+		
 	}
 
 }
